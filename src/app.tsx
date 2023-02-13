f3ab2fef13ce8c137948542bd92e3662a59b73b5
shijiePu <psj_email@163.com>
2023-02-13 15:15:26 +0800

    登录、登出

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,8 @@
 import { useRef, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { Layout } from "antd";
-import RouterNormal from "./RouterNormal";
+
 import RoutesV6 from "./RoutesV6";
-import Login from "./pages/login";
-import NarBar from "./pages/components/silder";
 
 import "./app.scss";
 const { Header, Footer, Content } = Layout;
@@ -16,33 +14,17 @@ const Appraisal = ({ children }: any) => {
 };
 
 function App() {
-  // const menuItems = matchRoutes()
-  // todo  通过matchRoutes实现拿到导航栏的数据？
-  const [login, setLogin] = useState(false);
-
   const hasUserInfo = useRef(sessionStorage.getItem("user"));
 
-  console.log({ hasUserInfo }, !hasUserInfo.current ? "Login" : "Layout ");
+  console.log(
+    "*************************",
+    { hasUserInfo },
+    !hasUserInfo.current ? "Login" : "Layout "
+  );
+
   return (
     <div className="App">
-      {!hasUserInfo.current ? (
-        <Login />
-      ) : (
-        <>
-          <Layout>
-            <NarBar />
-            <Layout className="app-content">
-              <Header className="app-header"></Header>
-              <Content className="contene-contain">
-                <RoutesV6 />
-              </Content>
-              <Footer className="app-footer">
-                <RouterNormal />
-              </Footer>
-            </Layout>
-          </Layout>
-        </>
-      )}
+      <RoutesV6 />
     </div>
   );
 }
